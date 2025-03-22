217560be6ee7dc88c80ce6cdb4107b7f1c47acc1
Temayoo <Matt.yo95280@gmail.com>
2025-03-22 17:38:16 +0100

    Header and Setup Project Finish

--- mon-portfolio/tailwind.config.js
+++ mon-portfolio/tailwind.config.js
@@ -1,11 +1,15 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Balmy: ['Balmy', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
