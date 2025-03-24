49fcc0d9636788c5025a5eaa3a9e5d32f611139f
Temayoo <Matt.yo95280@gmail.com>
2025-03-24 20:16:06 +0100

    left part of Home Page Finish

--- mon-portfolio/src/pages/Home.jsx
+++ mon-portfolio/src/pages/Home.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Accueil</h1>
-      <p className="mt-2">Bienvenue sur mon portfolio. Explorez mes projets, lisez mon blog et apprenez-en plus sur moi.</p>
+    <div>
+      <div className="w-full h-[500px] mx-auto flex">
+        <div className="w-[50%] h-full flex-col justify-center pl-40 pt-10">
+          <p className="font-SpaceMonoBoldItalic text-[#d4a384] text-base">Welcome...</p>
+          <h2 className="text-white font-LeagueSpartanBold text-8xl pt-5">RIVA</h2>
+          <h2 className="text-white font-LeagueSpartanBold text-8xl">Matteo . . .</h2>
+          <div className="flex items-center">
+          <p className="bg-[#d4a384] text-black font-SpaceMonoBoldItalic px-20 py-1 mt-5">WEB DEVELOPMENT</p>
+          </div>
+          <p className='text-white text-base pt-8'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet massa 
+            sed leo molestie ultrices in non mi. Suspendisse mollis nisl ac aliquet pretium. 
+            Suspendisse dignissim, odio non hendrerit suscipit, lorem ipsum tempus tortor, 
+            suscipit vestibulum velit quam nec nibh. 
+          </p>
+        </div>
+
+        <div className="w-[50%] h-full bg-blue-500 flex items-center justify-center"></div>
+      </div>
+
     </div>
   );
 };
--- mon-portfolio/tailwind.config.js
+++ mon-portfolio/tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
     extend: {
       fontFamily: {
         Balmy: ['Balmy', 'sans-serif'],
+        SpaceMono: ['Space Mono', 'sans-serif'],
+        SpaceMonoBoldItalic: ['Space Mono Bold Italic', 'sans-serif'],
+        LeagueSpartan: ['League Spartan', 'sans-serif'],
+        LeagueSpartanBold: ['League Spartan Bold', 'sans-serif'],
       },
     },
   },
