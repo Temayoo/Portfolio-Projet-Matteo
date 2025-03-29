0b486df4c9e76f45a56506c516ce0f177f781e62
Temayoo <Matt.yo95280@gmail.com>
2025-03-29 01:27:42 +0100

    organisation finish for about me juste need pics and text after that need to install gsap for animation

--- mon-portfolio/src/pages/About.jsx
+++ mon-portfolio/src/pages/About.jsx
@@ -5,7 +5,8 @@ import exampleImage from "../assets/pictures/me.png";
 const About = () => {
   return (
     <div className="w-full flex flex-col items-center relative">
-      <div className="w-full h-[100px] flex justify-center ">
+      {/* En-tête */}
+      <div className="w-full h-[100px] flex justify-center">
         <div className="w-[40%] relative">
           <p className="pl-[160px] pt-10 font-SpaceMonoBoldItalic text-2xl w-[70%] text-center text-white">
             WELL, WE SHOULD START <span className="text-[#d4a384]">HERE</span>
@@ -23,61 +24,108 @@ const About = () => {
         </div>
       </div>
 
-      <div className="w-full h-[150vh] flex justify-center mt-20 relative">
+      <div className="w-full h-[200vh] flex justify-center mt-20 relative">
+        {/* Ligne verticale centrale */}
         <svg
-          className="h-[1000px] absolute left-1/2"
+          className="h-[1500px] absolute left-1/2"
           width="2"
-          height="1000"
-          viewBox="0 0 2 1000"
+          height="1500"
+          viewBox="0 0 2 1500"
           fill="none"
           xmlns="http://www.w3.org/2000/svg">
           <line
             x1="1"
             y1="0"
             x2="1"
-            y2="1000"
+            y2="1500"
             stroke="#ffffff"
             strokeWidth="2"
           />
         </svg>
-        <div className="">
-          <div className="right-[calc(50%+50px)] top-[250px] text-right">
-            <p className="text-white font-semibold text-xl font-SpaceMonoBoldItalic">
-              2010 . .
-            </p>
-          </div>
 
-          <div className="right-1/2 top-[280px] left-[500px]">
-            <svg
-              width="300"
-              height="2"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <line
-                x1="0"
-                y1="1"
-                x2="300"
-                y2="1"
-                stroke="#ffffff"
-                strokeWidth="2"
-              />
-            </svg>
+        {/* Étape 1 - Droite */}
+        <div className="absolute top-0 w-full flex justify-center">
+          <div className="relative w-full max-w-[1000px]">
+            <div className="absolute right-[30%] w-[25%] h-[2px] bg-white">
+              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+                Étape 1
+              </div>
+            </div>
+            <div className="absolute right-[60%] top-1/2 -translate-y-1/2 w-[250px] text-white text-sm text-right">
+              <p>Je suis passionné de dev</p>
+            </div>
           </div>
+        </div>
 
-          <div className="left-[275px] top-[170px]">
-            <img
-              src={exampleImage}
-              alt="Image timeline 2010"
-              className="w-[200px] h-[200px] object-cover"
-            />
+        {/* Étape 2 - Gauche */}
+        <div className="absolute top-[250px] w-full flex justify-center">
+          <div className="relative w-full max-w-[1000px]">
+            <div className="absolute left-[30%] w-[25%] h-[2px] bg-white">
+              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+                Étape 2
+              </div>
+            </div>
+            <div className="absolute left-[60%] top-1/2 -translate-y-1/2 text-white text-sm w-[250px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit
+              amet massa sed leo molestie ultrices in non mi. Suspendisse mollis
+              nisl ac aliquet pretium.
+            </div>
           </div>
+        </div>
 
-          <div className="left-[calc(50%+100px)] top-[240px] w-[350px]">
-            <p className="text-white font-normal text-base">
-              Dès le plus jeune âge, j'ai développé une passion profonde pour
-              les écrans et les jeux vidéo, ce qui a fortement influencé ma
-              curiosité et mon intérêt pour l'univers digital.
-            </p>
+        {/* Étape 3 - Droite */}
+        <div className="absolute top-[500px] w-full flex justify-center">
+          <div className="relative w-full max-w-[1000px]">
+            <div className="absolute right-[30%] w-[25%] h-[2px] bg-white">
+              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+                Étape 3
+              </div>
+            </div>
+            <div className="absolute right-[60%] top-1/2 -translate-y-1/2 w-[250px] text-white text-sm text-right">
+              <p>J’ai appris Tailwind CSS</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Étape 4 - Gauche */}
+        <div className="absolute top-[750px] w-full flex justify-center">
+          <div className="relative w-full max-w-[1000px]">
+            <div className="absolute left-[30%] w-[25%] h-[2px] bg-white">
+              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+                Étape 4
+              </div>
+            </div>
+            <div className="absolute left-[60%] top-1/2 -translate-y-1/2 text-white text-sm w-[250px]">
+              <p>J’apprends aussi le design</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Étape 5 - Droite */}
+        <div className="absolute top-[1000px] w-full flex justify-center">
+          <div className="relative w-full max-w-[1000px]">
+            <div className="absolute right-[30%] w-[25%] h-[2px] bg-white">
+              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+                Étape 5
+              </div>
+            </div>
+            <div className="absolute right-[60%] top-1/2 -translate-y-1/2 w-[250px] text-white text-sm text-right">
+              <p>Je développe mon portfolio</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Étape 6 - Gauche */}
+        <div className="absolute top-[1250px] w-full flex justify-center">
+          <div className="relative w-full max-w-[1000px]">
+            <div className="absolute left-[30%] w-[25%] h-[2px] bg-white">
+              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+                Étape 6
+              </div>
+            </div>
+            <div className="absolute left-[60%] top-1/2 -translate-y-1/2 text-white text-sm w-[250px]">
+              <p>Je me lance sur le marché</p>
+            </div>
           </div>
         </div>
       </div>
