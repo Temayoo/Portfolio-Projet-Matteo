74fa63f871ae264c5087baa767d6d7c32a7a3895
Temayoo <Matt.yo95280@gmail.com>
2025-03-29 02:04:21 +0100

    just need gsap now style is good

--- mon-portfolio/src/pages/About.jsx
+++ mon-portfolio/src/pages/About.jsx
@@ -24,108 +24,136 @@ const About = () => {
         </div>
       </div>
 
-      <div className="w-full h-[200vh] flex justify-center mt-20 relative">
+      <div className="w-full h-[200vh] flex justify-center mt-20 pt-32 relative">
         {/* Ligne verticale centrale */}
         <svg
-          className="h-[1500px] absolute left-1/2"
-          width="2"
-          height="1500"
-          viewBox="0 0 2 1500"
+          className="h-[1400px] absolute left-1/2"
+          width="6"
+          height="1700"
+          viewBox="0 0 6 1700"
           fill="none"
           xmlns="http://www.w3.org/2000/svg">
           <line
-            x1="1"
+            x1="3"
             y1="0"
-            x2="1"
-            y2="1500"
+            x2="3"
+            y2="1700"
             stroke="#ffffff"
-            strokeWidth="2"
+            strokeWidth="6"
           />
         </svg>
 
-        {/* Étape 1 - Droite */}
-        <div className="absolute top-0 w-full flex justify-center">
+        {/* Étape 1 */}
+        <div className="absolute top-[128px] w-full flex justify-center">
           <div className="relative w-full max-w-[1000px]">
-            <div className="absolute right-[30%] w-[25%] h-[2px] bg-white">
-              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+            <div className="absolute right-[25%] w-[30%] h-[4px] bg-white">
+              <div className="absolute left-[40%] -top-[24px] text-white text-base whitespace-nowrap">
                 Étape 1
               </div>
             </div>
-            <div className="absolute right-[60%] top-1/2 -translate-y-1/2 w-[250px] text-white text-sm text-right">
+            <div className="absolute right-[60%] top-1/2 -translate-y-1/2 w-[270px] text-white text-base text-right">
               <p>Je suis passionné de dev</p>
             </div>
+            <img
+              src={exampleImage}
+              alt="Étape 1"
+              className="absolute right-[10%] top-1/2 -translate-y-1/2 w-[75px] h-[75px] rounded-full object-cover"
+            />
           </div>
         </div>
 
-        {/* Étape 2 - Gauche */}
-        <div className="absolute top-[250px] w-full flex justify-center">
+        {/* Étape 2 */}
+        <div className="absolute top-[408px] w-full flex justify-center">
           <div className="relative w-full max-w-[1000px]">
-            <div className="absolute left-[30%] w-[25%] h-[2px] bg-white">
-              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+            <div className="absolute left-[25%] w-[30%] h-[4px] bg-white">
+              <div className="absolute left-[40%] -top-[24px] text-white text-base whitespace-nowrap">
                 Étape 2
               </div>
             </div>
-            <div className="absolute left-[60%] top-1/2 -translate-y-1/2 text-white text-sm w-[250px]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit
-              amet massa sed leo molestie ultrices in non mi. Suspendisse mollis
-              nisl ac aliquet pretium.
+            <div className="absolute left-[60%] top-1/2 -translate-y-1/2 text-white text-base w-[270px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet massa sed leo molestie ultrices in non mi.
             </div>
+            <img
+              src={exampleImage}
+              alt="Étape 2"
+              className="absolute left-[10%] top-1/2 -translate-y-1/2 w-[75px] h-[75px] rounded-full object-cover"
+            />
           </div>
         </div>
 
-        {/* Étape 3 - Droite */}
-        <div className="absolute top-[500px] w-full flex justify-center">
+        {/* Étape 3 */}
+        <div className="absolute top-[688px] w-full flex justify-center">
           <div className="relative w-full max-w-[1000px]">
-            <div className="absolute right-[30%] w-[25%] h-[2px] bg-white">
-              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+            <div className="absolute right-[25%] w-[30%] h-[4px] bg-white">
+              <div className="absolute left-[40%] -top-[24px] text-white text-base whitespace-nowrap">
                 Étape 3
               </div>
             </div>
-            <div className="absolute right-[60%] top-1/2 -translate-y-1/2 w-[250px] text-white text-sm text-right">
+            <div className="absolute right-[60%] top-1/2 -translate-y-1/2 w-[270px] text-white text-base text-right">
               <p>J’ai appris Tailwind CSS</p>
             </div>
+            <img
+              src={exampleImage}
+              alt="Étape 3"
+              className="absolute right-[10%] top-1/2 -translate-y-1/2 w-[75px] h-[75px] rounded-full object-cover"
+            />
           </div>
         </div>
 
-        {/* Étape 4 - Gauche */}
-        <div className="absolute top-[750px] w-full flex justify-center">
+        {/* Étape 4  */}
+        <div className="absolute top-[968px] w-full flex justify-center">
           <div className="relative w-full max-w-[1000px]">
-            <div className="absolute left-[30%] w-[25%] h-[2px] bg-white">
-              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+            <div className="absolute left-[25%] w-[30%] h-[4px] bg-white">
+              <div className="absolute left-[40%] -top-[24px] text-white text-base whitespace-nowrap">
                 Étape 4
               </div>
             </div>
-            <div className="absolute left-[60%] top-1/2 -translate-y-1/2 text-white text-sm w-[250px]">
+            <div className="absolute left-[60%] top-1/2 -translate-y-1/2 text-white text-base w-[270px]">
               <p>J’apprends aussi le design</p>
             </div>
+            <img
+              src={exampleImage}
+              alt="Étape 4"
+              className="absolute left-[10%] top-1/2 -translate-y-1/2 w-[75px] h-[75px] rounded-full object-cover"
+            />
           </div>
         </div>
 
-        {/* Étape 5 - Droite */}
-        <div className="absolute top-[1000px] w-full flex justify-center">
+        {/* Étape 5 */}
+        <div className="absolute top-[1248px] w-full flex justify-center">
           <div className="relative w-full max-w-[1000px]">
-            <div className="absolute right-[30%] w-[25%] h-[2px] bg-white">
-              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+            <div className="absolute right-[25%] w-[30%] h-[4px] bg-white">
+              <div className="absolute left-[40%] -top-[24px] text-white text-base whitespace-nowrap">
                 Étape 5
               </div>
             </div>
-            <div className="absolute right-[60%] top-1/2 -translate-y-1/2 w-[250px] text-white text-sm text-right">
+            <div className="absolute right-[60%] top-1/2 -translate-y-1/2 w-[270px] text-white text-base text-right">
               <p>Je développe mon portfolio</p>
             </div>
+            <img
+              src={exampleImage}
+              alt="Étape 5"
+              className="absolute right-[10%] top-1/2 -translate-y-1/2 w-[75px] h-[75px] rounded-full object-cover"
+            />
           </div>
         </div>
 
-        {/* Étape 6 - Gauche */}
-        <div className="absolute top-[1250px] w-full flex justify-center">
+        {/* Étape 6  */}
+        <div className="absolute top-[1528px] w-full flex justify-center">
           <div className="relative w-full max-w-[1000px]">
-            <div className="absolute left-[30%] w-[25%] h-[2px] bg-white">
-              <div className="absolute left-[40%] -top-[20px] text-white text-sm whitespace-nowrap">
+            <div className="absolute left-[25%] w-[30%] h-[4px] bg-white">
+              <div className="absolute left-[40%] -top-[24px] text-white text-base whitespace-nowrap">
                 Étape 6
               </div>
             </div>
-            <div className="absolute left-[60%] top-1/2 -translate-y-1/2 text-white text-sm w-[250px]">
+            <div className="absolute left-[60%] top-1/2 -translate-y-1/2 text-white text-base w-[270px]">
               <p>Je me lance sur le marché</p>
             </div>
+            <img
+              src={exampleImage}
+              alt="Étape 6"
+              className="absolute left-[10%] top-1/2 -translate-y-1/2 w-[75px] h-[75px] rounded-full object-cover"
+            />
           </div>
         </div>
       </div>
